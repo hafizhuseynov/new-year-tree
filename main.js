eaefc454478f6a012e41a0a750bf6ebaac692e44
Hafiz Huseynov <hafiz.huseynov@wevo.az>
2023-12-26 12:28:45 +0400

    improved render quality by setting high DPI

--- main.js
+++ main.js
@@ -10,8 +10,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
 
-  canvas.style.backgroundColor = "#000066";
+  canvas.style.backgroundColor = "rgb(0 0 73)";
   canvas.style.display = "block";
+
+  let canvasWidth = canvas.width;
+  let canvasHeight = canvas.height;
 
   let colors = [
     "hsl(101, 69%, 88%)",
@@ -41,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
       lastColorChangeTime = timestamp;
     }
 
-    fillCanvasWithText(canvas.width, canvas.height);
+    fillCanvasWithText();
 
     requestAnimationFrame(animateCanvas);
   }
@@ -51,51 +54,54 @@ document.addEventListener("DOMContentLoaded", function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
+    setupHighDPI(canvas);
     const { fontSize, fontFamily } = window.getComputedStyle(
       document.documentElement
     );
 
-    ctx.font = fontSize + " " + fontFamily;
+    ctx.font = "bold " + fontSize + " " + fontFamily;
     lineHeight = parseInt(fontSize) * 1.2;
-    letterWidth = ctx.measureText("A").width
+    letterWidth = ctx.measureText("A").width;
+    canvasWidth = parseInt(canvas.style.width);
+    canvasHeight = parseInt(canvas.style.height);
   }
 
   // Animation loop
   function fillCanvasWithText() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvasWidth, parseInt(canvas.style.height));
 
     const next = wordIterator(text);
 
     let x = 0;
     let y = lineHeight;
 
-    while (y < canvas.height) {
+    while (y < parseInt(canvas.style.height)) {
       let letter = next();
       let currentLine = Math.round(y / lineHeight);
 
       // Triangle coordinates
-      let triangleHeight = Math.min(canvas.width / lineHeight / 1.2, 40);
+      let triangleHeight = Math.min(canvasWidth / lineHeight / 1.2, 40);
       let triangleStart = 5;
       let triangleEnd = triangleStart + triangleHeight;
 
       const isHeightMatch =
         currentLine >= triangleStart && currentLine <= triangleEnd;
 
-      const isWidthMatch =
-        x + letterWidth * (currentLine - triangleStart) > canvas.width / 2 &&
-        x - letterWidth * (currentLine - triangleStart) < canvas.width / 2;
+      const doesMatchTriangle =
+        isHeightMatch &&
+        x + letterWidth * (currentLine - triangleStart) > canvasWidth / 2 &&
+        x - letterWidth * (currentLine - triangleStart) < canvasWidth / 2;
 
-      ctx.fillStyle =
-        isHeightMatch && isWidthMatch
-          ? colors[currentLine % colors.length]
-          : "rgb(90, 90, 90)";
+      ctx.fillStyle = doesMatchTriangle
+        ? colors[currentLine % colors.length]
+        : "rgb(60, 60, 60)";
 
       ctx.fillText(letter, x, y);
 
       x += letterWidth;
 
       // go to next line, if there is not enought space
-      if (x + letterWidth > canvas.width) {
+      if (x + letterWidth > canvasWidth) {
         // color = colors[Math.floor(Math.random() * colors.length)];
         x = 0;
         y += lineHeight;
@@ -113,5 +119,31 @@ function wordIterator(word) {
   return () => {
     if (i >= word.length) i = 0;
     return word[i++];
-  }
+  };
+}
+
+// https://stackoverflow.com/a/15666143/10629172
+function setupHighDPI(canvas) {
+  let ctx = canvas.getContext("2d");
+  let dpr = window.devicePixelRatio || 1;
+  let bsr =
+    ctx.webkitBackingStorePixelRatio ||
+    ctx.mozBackingStorePixelRatio ||
+    ctx.msBackingStorePixelRatio ||
+    ctx.oBackingStorePixelRatio ||
+    ctx.backingStorePixelRatio ||
+    1;
+
+  let ratio = dpr / bsr;
+
+  let oldWidth = canvas.width;
+  let oldHeight = canvas.height;
+
+  canvas.width = oldWidth * ratio;
+  canvas.height = oldHeight * ratio;
+
+  canvas.style.width = oldWidth + "px";
+  canvas.style.height = oldHeight + "px";
+
+  ctx.scale(ratio, ratio);
 }
